a7c4a723201d8832525e18086070f48fa410fdf7
Avalon-950427 <m17625935728@163.com>
2021-06-11 17:46:24 +0800

    新增事件

--- record/utils.js
+++ record/utils.js
@@ -77,3 +77,25 @@ export class BrowserDetector {
     isSafari() { return this.isSafari_Gte3Lte9_1 || this.isSafari_Gte7_1; }
     isOpera() { return this.isOpera_Gte20; }
 }
+
+export let EventUtil = {
+    addHandler(element,type,handler){
+        if(element.addEventListener){
+            element.addEventListener(type,handler,false)
+        }else if(element.attachEvent){
+            element.attachEvent('on'+type,handler)
+        }else{
+            element['on'+type] = handler
+        }
+    },
+
+    removeHandler(element,type,handler){
+        if(element.removeEventListener){
+            element.removeEventListener(type,handler,false)
+        }else if(element.detachEvent){
+            element.detachEvent('on'+type,handler)
+        }else{
+            element['on'+type] = null
+        }
+    }
+}
